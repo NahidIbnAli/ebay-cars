4fb6aa3bcd8b3d9b8d8e5c064b4481c5fbbbd48c
Nahid Ibn Ali <nahidibnali7@gmail.com>
2022-11-25 11:58:31 +0600

    added hero section to the home page

---
+++ src/pages/Home/Hero/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div
+      className="hero "
+      style={{ backgroundImage: `url("heroImage.jpg")`, minHeight: "500px" }}
+    >
+      <div className="hero-overlay bg-opacity-80"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="hero-text text-white">
+          <h1 className="mb-2 text-5xl font-bold leading-tight">
+            Find the Car You Want, Your Way
+          </h1>
+          <p className="mb-8 text-xl">
+            Then, build your deal to fit your needs.
+          </p>
+          <button className="btn btn-primary text-white">Shop Used</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
